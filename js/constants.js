f3b2748a70e747b8e9290021f9248b126c5433ed
Siljesandersen <silje_sandersen@hotmail.com>
2021-11-23 12:57:54 +0100

    testing

--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,3 @@
 export const productsUrl = "https://strapi-rkkq.onrender.com/products/";
 export const heroUrl = "https://strapi-rkkq.onrender.com/hero";
+export const heroBanner = document.querySelector(".herobanner");
