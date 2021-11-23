f5fe1575938d1ebdd74a541502651c4bd21018e3
Siljesandersen <silje_sandersen@hotmail.com>
2021-11-23 12:50:08 +0100

    working with js

--- js/components/api.js
+++ js/components/api.js
@@ -2,3 +2,10 @@ export async function getProducts(url) {
     const response = await fetch(url);
     const json = await response.json();
 }
+
+export async function getHero(url) {
+    const response = await fetch(url);
+    const json = await response.json();
+
+    
+}
