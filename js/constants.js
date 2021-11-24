f212680a88661f7fac89b80a4f61fe9d67602444
Siljesandersen <silje_sandersen@hotmail.com>
2021-11-24 10:52:48 +0100

    creating card

--- js/constants.js
+++ js/constants.js
@@ -1,3 +1,4 @@
 export const productsUrl = "https://strapi-rkkq.onrender.com/products/";
 export const heroUrl = "https://strapi-rkkq.onrender.com/hero";
 export const heroBanner = document.querySelector(".herobanner");
+export const featuredProducts = document.querySelector(".featured__products");
